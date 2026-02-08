3a5e805aace08c4e1d1aa0f54c7969c582785671
bakunzifiston <82505728+bakunzifiston@users.noreply.github.com>
2026-02-08 12:11:04 +0200

    change

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('title', config('app.name', 'Laravel'))</title>
+        <title>@yield('title', 'AgriHub')</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=dm-sans:400,500,600,700" rel="stylesheet" />
@@ -16,7 +16,7 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-stone-100">
             <div>
                 <a href="{{ route('home') }}" class="block">
-                    <span class="text-2xl font-semibold text-emerald-800">Tenant Platform</span>
+                    <span class="text-2xl font-semibold text-emerald-800">AgriHub</span>
                 </a>
             </div>
 
